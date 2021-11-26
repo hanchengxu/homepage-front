a3e90cdf28936ec88b20f0ecf1ccbcd591c7acae
hanchengxu <chengxuhan123@126.com>
2021-11-26 11:07:19 +0900

    webpack setting

--- vue.config.js
+++ vue.config.js
@@ -40,18 +40,40 @@ module.exports = {
          return args
        })  
     config.resolve.alias.set('vue-i18n', 'vue-i18n/dist/vue-i18n.cjs.js')
+    config.optimization.splitChunks({
+      chunks: "all",          //async异步代码分割 initial同步代码分割 all同步异步分割都开启
+      minSize:  500000,         //字节 引入的文件大于30kb才进行分割
+      maxSize: 500000,         //100kb，尝试将大于100kb的文件拆分成n个100kb的文件
+      minChunks: 1,           //模块至少使用次数
+      maxAsyncRequests: 5,    //同时加载的模块数量最多是5个，只分割出同时引入的前5个文件
+      maxInitialRequests: 3,  //首页加载的时候引入的文件最多3个
+      automaticNameDelimiter: '~', //缓存组和生成文件名称之间的连接符
+      name: true,                  //缓存组里面的filename生效，覆盖默认命名
+      cacheGroups: { //缓存组，将所有加载模块放在缓存里面一起分割打包
+        vendors: {  //自定义打包模块
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10, //优先级，先打包到哪个组里面，值越大，优先级越高
+          // filename: 'vendors.js',
+        },
+        // default: { //默认打包模块
+        //   priority: -20,
+        //   reuseExistingChunk: true, //模块嵌套引入时，判断是否复用已经被打包的模块
+        //   // filename: 'common.js'
+        // }
+      }
+    });
   },
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
       // 为生产环境修改配置...
       config.mode = 'production'
-      return{
-        plugins: [new CompressionPlugin({
-          test: /\.js$|\.html$|\.css/,//压缩文件名
-          threshold: 10240,//超过10K 压缩
-          deleteOriginalAssets: false
-        })]
-      }
+      // return{
+      //   plugins: [new CompressionPlugin({
+      //     test: /\.js$|\.html$|\.css/,//压缩文件名
+      //     threshold: 10240,//超过10K 压缩
+      //     deleteOriginalAssets: false
+      //   })]
+      // }
     } else {
       // 为开发环境修改配置...
       config.mode = 'development'
