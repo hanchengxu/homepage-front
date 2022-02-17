7b59241b611b680225e7f6b4f2bcf4def2ee7121
hanchengxu <chengxuhan123@126.com>
2022-02-17 18:21:41 +0900

    upgrade vue cli

--- vue.config.js
+++ vue.config.js
@@ -111,7 +111,7 @@ module.exports = {
     //   css: {}, // 这里的选项会传递给 css-loader
     //   postcss: {} // 这里的选项会传递给 postcss-loader
     // }, // css预设器配置项 详见https://cli.vuejs.org/zh/config/#css-loaderoptions
-    modules: false // 启用 CSS modules for all css / pre-processor files.
+    requireModuleExtension: false // 启用 CSS modules for all css / pre-processor files.
   },
   parallel: require('os').cpus().length > 1, // 是否为 Babel 或 TypeScript 使用 thread-loader。该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建。
   pwa: {}, // PWA 插件相关配置 see https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
