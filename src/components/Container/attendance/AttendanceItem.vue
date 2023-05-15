f0dafff7cedbef76f4a29d2309eddba1b5b1b734
hanchengxu <100873808@qq.com>
2023-05-15 21:29:49 +0900

    report page

---
+++ src/components/Container/attendance/AttendanceItem.vue
@@ -0,0 +1,29 @@
+<template>
+        <div v-for="(item,index) in itemList" :key="item.itemName" class="d-flex flex-row justify-content-center">
+            <div class="col-6 text-end pe-5">{{ item.itemName }}</div>
+            <div class="col-6">
+                <vue3-autocounter ref='counter' :startAmount='0' :endAmount=item.itemValue 
+                    :duration='1' prefix='￥' suffix='円'
+                    separator=',' :decimals='0' :autoinit='true' />
+            </div>  
+        </div>
+</template>
+<script>
+import { postAPI } from '@/utils.js';
+import moment from 'moment';
+export default {
+name: 'AttendanceItem', 
+props: {
+    itemList: {require: false, default: []}
+},
+data() {
+    return {
+    }
+  },  
+methods: {
+  }
+
+}
+</script>
+<style scoped>
+</style>
--- src/components/Container/attendance/AttendanceReport.vue
+++ src/components/Container/attendance/AttendanceReport.vue
@@ -1,15 +1,23 @@
 <template>
-    <div>
-        <div class="pt-3 px-5 d-flex justify-content-between align-items-center">
-            <div class="">月报({{ now }})</div>
+    <div class="attendance-main pt-5">
+        <div class="py-3 mx-1 mx-xxl-5 px-4 d-flex justify-content-end align-items-center attendance-title rounded-pill">
+            <div class="me-auto fw-bold fs-2">月报</div>
             <i class="menu bi bi-arrow-return-left" style="font-size: 2rem;" @click="backAttendance"></i>
         </div>
-        <div class="px-xxl-5 px-xl-5 px-lg-5 d-flex flex-xxl-row flex-xl-row flex-lg-row flex-md-column flex-sm-column flex-column">
-            <vue3-autocounter ref='counter' :startAmount='0' :endAmount='5000000' 
-            :duration='1' prefix='￥' suffix='円' separator=','
-            :decimals='0' :autoinit='true'/>
+        <div class="report">
+            <div class="mt-5 py-4 px-xxl-5 px-xl-5 px-lg-5 d-flex flex-column justify-content-center">
+                <AttendanceItem :itemList=itemList />
+                <div class="mt-2 border-top border-2 border-primary"></div>
+                <div class="d-flex flex-row justify-content-between">
+                    <span class="col-6 ps-4 sum">合计</span>
+                    <span class="col-6">
+                        <vue3-autocounter ref='counter' :startAmount='0' :endAmount='4000' 
+                            :duration='1' prefix='￥' suffix='円'
+                            separator=',' :decimals='0' :autoinit='true' />
+                    </span>  
+                </div>
+            </div>
         </div>
-        <div class="px-5 ms-1" @click="addingMaster"><i class="bi bi-plus-square add-icon"></i></div>
     </div>
 </template>
 <script>
@@ -17,14 +25,22 @@ import { onMounted, ref, onUnmounted } from "vue";
 import { getAPI, postAPI, isNull } from '@/utils.js';
 import moment from 'moment';
 import { find, findIndex } from 'lodash'
+import AttendanceItem from "./AttendanceItem.vue";
 export default {
     name: "AttendanceReport",
     components: {
+        AttendanceItem
     },
     data() {
         return {
             value: 1000,
-            now: moment().format('YYYY-MM-DD')
+            now: moment().format('YYYY-MM-DD'),
+            itemList: [
+                {itemName:'项目1',itemValue:5000},
+                {itemName:'项目2',itemValue:15000},
+                {itemName:'项目3',itemValue:2000},
+                {itemName:'项目4',itemValue:4000},
+            ]
         }
     },
     setup() {
@@ -34,7 +50,7 @@ export default {
             return window.innerHeight;
         };
         const getScollWidth = () => {
-            return window.innerWidth;y
+            return window.innerWidth; y
         };
         const scollHeight = ref(getScollHeight());
         const width = ref(getScollWidth());
@@ -64,13 +80,30 @@ export default {
         startAnimate: function () {
             this.$refs.myNum.start()
         },
-        backAttendance(){
-            this.$router.push({ path: 'attendance'});
+        backAttendance() {
+            this.$router.push({ path: 'attendance' });
         }
     }
 };
 </script>
 <style scoped>
+.report {
+    color: white;
+    font-weight: bold;
+    font-size: 1.4rem;
+    -webkit-text-stroke: 1px #000000;
+    background: white;
+    margin-left: 50px;
+    margin-right: 50px;
+    opacity: 0.95;
+    border-radius: 10px;
+}
+
+.sum {
+    color: black;
+    -webkit-text-stroke: 0px black;
+}
+
 .menu {
     cursor: pointer;
     transform: scale(1);
