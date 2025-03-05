c06c573c79d5d038a9f8c79bfaee5f6131c59869
chengxu.han <100873808@qq.com>
2025-03-05 13:36:43 +0900

    reset config

--- vue.config.js
+++ vue.config.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   // publicPath: '/', // 基本路径
   //github会改变根路径，配合github page 默认域名。但自定义路由后不必使用
-  publicPath: process.env.NODE_ENV === 'github' ? '/homepage-front/' : './',
+  publicPath: process.env.NODE_ENV === 'github' ? '/homepage-front/' : '/',
   outputDir: 'dist', // 输出文件目录
   lintOnSave: false, // eslint-loader 是否在保存的时候检查
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
